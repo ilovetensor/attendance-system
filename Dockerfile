f04468261a800eddaa37f33215b669e77bb68221
ilovetensor <rohanshar11@gmail.com>
2023-03-03 19:55:29 +0530

    attendence system uploaded v-1

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,3 @@
-
-
 FROM continuumio/anaconda3
 COPY . /user/app/
 EXPOSE 5000
@@ -9,8 +7,6 @@ RUN pip install -r requirements.txt && \
     pip install cmake && \
     conda install -c conda-forge dlib && \
     pip install face_recognition
-    # pip install https://pypi.python.org/packages/da/06/bd3e241c4eb0a662914b3b4875fc52dd176a9db0d4a2c915ac2ad8800e9e/dlib-19.7.0-cp36-cp36m-win_amd64.whl#md5=b7330a5b2d46420343fbed5df69e6a3f && \
+    
 
-
-
-CMD python hello.py
+CMD python main.py
---
+++ main.py
@@ -0,0 +1,194 @@
+import face_recognition
+import cv2
+import numpy as np
+from datetime import datetime, timedelta
+import os 
+import pandas as pd
+
+global gen_frame
+gen_frame = True 
+
+from flask import Flask, render_template, Response, request, redirect, flash, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'ImagesAttendance'
+
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+
+
+# ======================= encoding ========================
+
+path = 'ImagesAttendance'
+images = []
+classNames = []
+myList = os.listdir(path)
+print(myList)
+
+for cl in myList:
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+
+# print(classNames)
+
+
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+def markAttendence(name):
+    with open('Data.csv', 'r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        timeList = []
+        dateList = []
+
+        for line in myDataList:
+            entry = line.split(',') 
+            nameList.append(entry[0])
+            timeList.append(entry[1])
+            dateList.append(entry[2])
+            # print(entry)
+        # print(dateList)
+        if name not in nameList and name:
+            now = datetime.now()
+            date = now.strftime('%d/%m/%Y')
+            dtString = now.strftime('%H:%M:%S')
+            f.writelines(f'\n{name}, {dtString}, {date}')
+            return 
+        else:
+            nameList.reverse()
+            timeList.reverse()
+            dateList.reverse()
+            recent = nameList.index(name)
+            time = timeList[recent].split(':')
+            for i in range(len(time)): time[i] = int(time[i])
+            date = dateList[recent].split('/')
+            date[2] = date[2][:4]
+            for i in range(len(date)): date[i] = int(date[i])
+            dt = datetime(date[2],date[1],date[0],*time)
+            now = datetime.now()
+            # print(dt)
+            if (now-dt) > timedelta(1,0,0):
+                date = now.strftime('%d/%m/%Y')
+                dtString = now.strftime('%H:%M:%S')
+                f.writelines(f'\n{name}, {dtString}, {date}')
+                return 
+
+
+          
+
+
+encodeListKnown = findEncodings(images)
+print('Encoding Done')
+
+
+# =========================--------=========================
+
+
+
+def gen_frames():
+    camera = cv2.VideoCapture(0)
+    while True and gen_frame:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+
+            success, img = camera.read()
+            imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+            imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+            # encode = face_recognition.face_encodings(img)[0]
+            facesCurFrame = face_recognition.face_locations(imgS)
+            # print(gen_frame)
+            encodesCurFrame = face_recognition.face_encodings(imgS)
+
+            for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+                matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+                faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+                # print(faceDis)
+                matchIndex = np.argmin(faceDis)
+
+                if matches[matchIndex]:
+                    name = classNames[matchIndex].upper()
+                    # print(name)
+                    y1, x2, y2, x1 = faceLoc
+                    y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
+                    cv2.rectangle(img, (x1,y1), (x2,y2), (0,255,0), 2)
+                    cv2.rectangle(img, (x1,y2-35), (x2,y2), (0,255,0), cv2.FILLED)
+                    cv2.putText(img, name, (x1+6,y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 2)
+
+                    markAttendence(name)
+
+            ret, buffer = cv2.imencode('.jpg', img)
+            frame = buffer.tobytes()
+
+            yield(b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            
+
+@app.route('/')
+def index():
+    global gen_frame
+    gen_frame = False
+    return render_template('index.html')
+
+@app.route('/capture')
+def capture():
+    global gen_frame
+    gen_frame = False
+    return render_template('capture.html')
+
+
+@app.route('/video_feed')
+def video_feed():
+   global gen_frame
+   gen_frame = True
+   return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/upload')
+def upload_file():
+    global gen_frame
+    gen_frame = False
+    return render_template('upload.html')
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def pload_file():
+    global gen_frame
+    gen_frame = False
+    def allowed_file(filename): return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+
+        if file.filename == '': 
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return redirect('/')
+
+@app.route('/dataset')
+def show_data():
+    global gen_frame
+    gen_frame = False
+    with open('Data.csv', 'r+') as f:
+        myDataList = f.readlines()
+        
+    return render_template('data.html', value=myDataList)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
